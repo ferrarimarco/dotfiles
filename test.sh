e4e86af191d6a63eea048737da800ae67ea3ba5a
Marco Ferrari <ferrari.marco@gmail.com>
2018-08-03 10:25:00 +0000

    Enable test for dotfiles

--- test.sh
+++ test.sh
@@ -2,11 +2,22 @@
 
 set -euo pipefail
 
+# running in alpine
+# manually install needed package as I don't want to manage
+# a custom shellcheck image
+if which apk ; then
+  apk add --update --no-cache \
+    file
+fi
+
 # find all executables and run `shellcheck`
-find . -type f -iname "*.sh" -not -iname '*.git*' | sort -u | while read -r f; do
-  if shellcheck "$f"; then
-    echo "[OK]: sucessfully linted $f"
-  else
-    exit 1
-  fi
+find . -type f -not -path "*/\.git/*" | sort -u | while read -r f; do
+  echo "File: $f"
+  if file "$f" | grep -q shell; then
+    if shellcheck "$f"; then
+      echo "[OK]: sucessfully linted $f"
+    else
+      exit 1
+    fi
+	fi
 done
