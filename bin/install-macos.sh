d0542e2c8ef35983cb2dff127d5b9c99150bc90b
Marco Ferrari <ferrari.marco@gmail.com>
2019-03-28 17:35:12 +0100

    Simplified formulae installation

--- bin/install-macos.sh
+++ bin/install-macos.sh
@@ -61,48 +61,48 @@ install_brew_formulae() {
 	# Save Homebrew’s installed location.
 	BREW_PREFIX="$(brew --prefix)"
 
-	# Install GNU core utilities (those that come with macOS are outdated).
-	# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-	brew install coreutils
+	for f in \
+		bash \
+		bash-completion \
+		coreutils \
+		findutils \
+		git \
+		git-lfs \
+		gnupg \
+		gnu-sed \
+		grep \
+		imagemagick \
+		moreutils \
+		openssh \
+		screen \
+		php \
+		p7zip \
+		tree \
+		vim \
+		wget
+  do
+    if brew ls --versions "$f" > /dev/null; then
+      brew install "$f"
+    fi
+  done
+
+	echo 'Add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH` if you would prefer coreutils be the defaults.'
+	echo 'Add `$(brew --prefix findutils)/libexec/gnubin` to `$PATH` if you would prefer findutils be the defaults.'
+	echo 'Add `$(brew --prefix gnu-sed)/libexec/gnubin` to `$PATH` if you would prefer gnu-sed be the defaults.'
+
+	echo 'Mapping gsha256sum to sha256sum'
 	ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
-	# Install some other useful utilities like `sponge`.
-	brew install moreutils
-	# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-	brew install findutils
-	# Install GNU `sed`, overwriting the built-in `sed`.
-	brew install gnu-sed --with-default-names
-	# Install Bash 4.
-	brew install bash
-	brew install bash-completion@2
-
-	# Switch to using brew-installed bash as default shell
+	echo "Switching to using brew-installed bash as default shell"
 	if ! grep -Fq "${BREW_PREFIX}/bin/bash" /etc/shells; then
 		echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
 		chsh -s "${BREW_PREFIX}/bin/bash";
 	fi;
 
-	# Install `wget` with IRI support.
-	brew install wget --with-iri
+	echo 'Mapping vi so it opens the brew-installed vim'
+	ln -s /usr/local/bin/vim /usr/local/bin/vi
 
-	# Install GnuPG to enable PGP-signing commits.
-	brew install gnupg
-
-	# Install more recent versions of some macOS tools.
-	brew install vim --with-override-system-vi
-	brew install grep
-	brew install openssh
-	brew install screen
-	brew install homebrew/php/php56 --with-gmp
-
-	# Install other useful binaries.
-	brew install git
-	brew install git-lfs
-	brew install imagemagick --with-webp
-	brew install p7zip
-	brew install tree
-
-	# Remove outdated versions from the cellar.
+	echo "Removing outdated versions from the cellar."
 	brew cleanup
 }
 
@@ -117,7 +117,8 @@ patch_brew(){
 }
 
 update_brew() {
-	brew update
+	echo "The following formulae are outdated: $(brew outdated)"
+
 	brew upgrade
 
 	# Check if we need to patch homebrew (if it was updated)
