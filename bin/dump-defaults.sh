2b3838d72ab734b19f0bb118f5d24db838ad8020
Marco Ferrari <ferrari.marco@gmail.com>
2019-08-07 09:20:24 +0200

    Print before/after when searching for defaults

--- bin/dump-defaults.sh
+++ bin/dump-defaults.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-defaults read NSGlobalDomain > NSGlobalDomain.out
-defaults -currentHost read NSGlobalDomain > NSGlobalDomain-currentHost.out
-defaults read > read.out
-defaults -currentHost read > read-currentHost.out
+defaults read NSGlobalDomain > NSGlobalDomain-before.out
+defaults -currentHost read NSGlobalDomain > NSGlobalDomain-currentHost-before.out
+defaults read > read-before.out
+defaults -currentHost read > read-currentHost-before.out
+
+read -n1 -rsp "Change the settings and press any key to continue...\n" key
+
+defaults read NSGlobalDomain > NSGlobalDomain-after.out
+defaults -currentHost read NSGlobalDomain > NSGlobalDomain-currentHost-after.out
+defaults read > read-after.out
+defaults -currentHost read > read-currentHost-after.out
