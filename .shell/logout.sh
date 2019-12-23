49670fbb5c0cb6eaef92b24f1e1a79b1b40d3873
Marco Ferrari <ferrari.marco@gmail.com>
2019-12-23 15:24:16 +0100

    Ignore SC2039 in .shell/logout.sh

--- .shell/logout.sh
+++ .shell/logout.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # when leaving the console clear the screen to increase privacy
+# shellcheck disable=SC2039  # Ignoring this so this script can be shell-agnostic
 if [ "$SHLVL" = 1 ]; then
     [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
     [ -x /usr/bin/clear ] && /usr/bin/clear
