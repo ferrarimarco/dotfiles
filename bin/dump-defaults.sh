d8acdcca9b73ef85e8f504151fbfb1a9c7b68d57
Marco Ferrari <ferrari.marco@gmail.com>
2020-01-28 18:26:59 +0100

    Add echoes and diffs when dumping defaults

--- bin/dump-defaults.sh
+++ bin/dump-defaults.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-defaults read NSGlobalDomain > NSGlobalDomain-before.out
-defaults -currentHost read NSGlobalDomain > NSGlobalDomain-currentHost-before.out
-defaults read > read-before.out
-defaults -currentHost read > read-currentHost-before.out
+echo "Reading defaults..."
+defaults read NSGlobalDomain >NSGlobalDomain-before.out
+defaults -currentHost read NSGlobalDomain >NSGlobalDomain-currentHost-before.out
+defaults read >read-before.out
+defaults -currentHost read >read-currentHost-before.out
 
 read -n1 -rsp $'Change the settings and press any key to continue...\n'
 
-defaults read NSGlobalDomain > NSGlobalDomain-after.out
-defaults -currentHost read NSGlobalDomain > NSGlobalDomain-currentHost-after.out
-defaults read > read-after.out
-defaults -currentHost read > read-currentHost-after.out
+defaults read NSGlobalDomain >NSGlobalDomain-after.out
+defaults -currentHost read NSGlobalDomain >NSGlobalDomain-currentHost-after.out
+defaults read >read-after.out
+defaults -currentHost read >read-currentHost-after.out
+
+echo "Diffing..."
+diff NSGlobalDomain-before.out NSGlobalDomain-after.out
+diff NSGlobalDomain-currentHost-before.out NSGlobalDomain-currentHost-after.out
+diff read-currentHost-before.out read-currentHost-after.out
