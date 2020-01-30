496fb12386ddb1ab600e699089fed8bed55b8d9d
Marco Ferrari <ferrari.marco@gmail.com>
2020-01-30 19:01:59 +0100

    Init brew vars only if necessary. Close #18

--- .shells/.all/environment.sh
+++ .shells/.all/environment.sh
@@ -27,25 +27,32 @@ export PATH=/usr/local/bin:${PATH}:/sbin
 export PATH=$HOME/bin:$PATH
 
 # setup homebrew environment
-export HOMEBREW_REPOSITORY=/usr/local/Homebrew
-export HOMEBREW_PATH=/usr/local/brew
-export PATH="${HOMEBREW_PATH}"/bin:"${PATH}"
-export PATH="${HOMEBREW_PATH}"/sbin:"${PATH}"
-export LD_LIBRARY_PATH="${HOMEBREW_PATH}"/lib:"${LD_LIBRARY_PATH}"
-export MANPATH="${HOMEBREW_PATH}"/share/man:"${MANPATH}"
-export HOMEBREW_NO_ANALYTICS=1
+HOMEBREW_PATH=/usr/local/brew
+if [ -d "${HOMEBREW_PATH}" ]; then
+    export HOMEBREW_REPOSITORY=/usr/local/Homebrew
+    export HOMEBREW_PATH
+    export PATH="${HOMEBREW_PATH}"/bin:"${PATH}"
+    export PATH="${HOMEBREW_PATH}"/sbin:"${PATH}"
+    export LD_LIBRARY_PATH="${HOMEBREW_PATH}"/lib:"${LD_LIBRARY_PATH}"
+    export MANPATH="${HOMEBREW_PATH}"/share/man:"${MANPATH}"
+    export HOMEBREW_NO_ANALYTICS=1
+
+    # Uncomment the lines below if you want to use executables installed with Homebrew
+    # instead of the macOS ones
+    #export PATH="${HOMEBREW_PATH}"/opt/coreutils/libexec/gnubin:${PATH}
+    #export MANPATH="${HOMEBREW_PATH}"/opt/coreutils/libexec/gnuman:${MANPATH}
+    #export PATH="${HOMEBREW_PATH}"/opt/make/libexec/gnubin:${PATH}
+    #export MANPATH="${HOMEBREW_PATH}"/opt/make/libexec/gnuman:${MANPATH}
+    #export PATH="${HOMEBREW_PATH}"/opt/findutils/libexec/gnubin:${PATH}
+    #export MANPATH="${HOMEBREW_PATH}"/opt/findutils/libexec/gnuman:${MANPATH}
+else
+    unset HOMEBREW_PATH
+fi
 
 # add vs code bins to path
 VS_CODE_PATH_MACOS="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/"
 if [ -d "${VS_CODE_PATH_MACOS}" ]; then
     export PATH="${VS_CODE_PATH_MACOS}:${PATH}"
+else
+    unset VS_CODE_PATH_MACOS
 fi
-
-# Uncomment the lines below if you want to use executables installed with Homebrew
-# instead of the macOS ones
-#export PATH="${HOMEBREW_PATH}"/opt/coreutils/libexec/gnubin:${PATH}
-#export MANPATH="${HOMEBREW_PATH}"/opt/coreutils/libexec/gnuman:${MANPATH}
-#export PATH="${HOMEBREW_PATH}"/opt/make/libexec/gnubin:${PATH}
-#export MANPATH="${HOMEBREW_PATH}"/opt/make/libexec/gnuman:${MANPATH}
-#export PATH="${HOMEBREW_PATH}"/opt/findutils/libexec/gnubin:${PATH}
-#export MANPATH="${HOMEBREW_PATH}"/opt/findutils/libexec/gnuman:${MANPATH}
