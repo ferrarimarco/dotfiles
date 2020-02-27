83c060d169b147f3cd5fae0eb23d4a173b4b313b
Marco Ferrari <ferrari.marco@gmail.com>
2020-02-27 19:52:46 +0100

    Fix VScode settings link on win

--- bin/install-windows.ps1
+++ bin/install-windows.ps1
@@ -1,19 +1,18 @@
-function Install-Chocolatey
-{
-  [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingInvokeExpression", "", Justification="Trusting Chocolatey installer")]
-  param()
+function Install-Chocolatey {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingInvokeExpression", "", Justification = "Trusting Chocolatey installer")]
+    param()
 
-  if (!$env:ChocolateyInstall) {
-    Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-  } else {
-    Write-Output "Chocolatey is already installed. Upgrading..."
-    choco upgrade -y chocolatey
-  }
+    if (!$env:ChocolateyInstall) {
+        Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+    else {
+        Write-Output "Chocolatey is already installed. Upgrading..."
+        choco upgrade -y chocolatey
+    }
 }
 
-function Install-Packages
-{
-  $Packages =
+function Install-Packages {
+    $Packages =
     'conemu',
     'git',
     'keepass',
@@ -23,26 +22,22 @@ function Install-Packages
     'vlc',
     'vscode'
 
-  ForEach ($Package in $Packages)
-  {
-    choco install -y $Package
-  }
+    ForEach ($Package in $Packages) {
+        choco install -y $Package
+    }
 }
 
-function Initialize-VSCode
-{
-    mklink "%APPDATA%\Code\settings.json" "%HOMEDRIVE%%HOMEPATH%\workspaces\dotfiles\.config\Code\User\settings.json"
+function Initialize-VSCode {
+    mklink "%APPDATA%\Code\User\settings.json" "%HOMEDRIVE%%HOMEPATH%\workspaces\dotfiles\.config\Code\User\settings.json"
 }
 
-function Install-VSCode-Extensions
-{
-  Get-Content "..\.config\Code\extensions.txt" | ForEach-Object {
-    code --install-extension $_
-  }
+function Install-VSCode-Extensions {
+    Get-Content "..\.config\Code\extensions.txt" | ForEach-Object {
+        code --install-extension $_
+    }
 }
 
-function Install-WSL
-{
+function Install-WSL {
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
     Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile Ubuntu.appx -UseBasicParsing
     Add-AppxPackage .\Ubuntu.appx
