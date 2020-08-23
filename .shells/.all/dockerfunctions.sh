650e049b82046696c21b97e6d14529b64d930223
Marco Ferrari <ferrari.marco@gmail.com>
2020-08-23 17:57:15 +0200

    Fix docker alias options

--- .shells/.all/dockerfunctions.sh
+++ .shells/.all/dockerfunctions.sh
@@ -2,10 +2,9 @@
 
 # Wrappers for docker run commands
 
-DOCKER_FLAGS="-i"
-DOCKER_FLAGS="$DOCKER_FLAGS --rm"
+DOCKER_TTY_OPTION=
 if [ -t 0 ]; then
-    DOCKER_FLAGS="$DOCKER_FLAGS -t"
+    DOCKER_TTY_OPTION="-t"
 fi
 
 #
@@ -42,78 +41,85 @@ relies_on() {
 ansible() {
     del_stopped ansible
     # shellcheck disable=SC2086
-    docker run \
-        $DOCKER_FLAGS \
+    docker run "$DOCKER_TTY_OPTION" \
+        -i \
         --net=host \
         -v /etc/localtime:/etc/localtime:ro \
         -v "$(pwd)":/etc/ansible \
         -v "${HOME}"/.ssh:/root/.ssh:ro \
         --name ansible \
+        --rm \
         ferrarimarco/open-development-environment-ansible "$@"
 }
 
 changelog_generator() {
     del_stopped changelog-generator
     # shellcheck disable=SC2086
-    docker run \
-        $DOCKER_FLAGS \
+    docker run "$DOCKER_TTY_OPTION" \
+        -i \
         -v /etc/localtime:/etc/localtime:ro \
         -v "$(pwd)":/usr/local/src/your-app \
         --name changelog-generator \
+        --rm \
         ferrarimarco/github-changelog-generator:1.15.0 "$@"
 }
 
 docker_clean() {
     del_stopped docker-clean
     # shellcheck disable=SC2086
-    docker run \
-        $DOCKER_FLAGS \
+    docker run "$DOCKER_TTY_OPTION" \
+        -i \
         -v /etc/localtime:/etc/localtime:ro \
         -v /var/run/docker.sock:/var/run/docker.sock \
         --name docker-clean \
+        --rm \
         zzrot/docker-clean "$@"
 }
 
 jq() {
     del_stopped jq
     # shellcheck disable=SC2086
-    docker run \
-        $DOCKER_FLAGS \
+    docker run "$DOCKER_TTY_OPTION" \
+        -i \
         --name jq \
+        --rm \
         stedolan/jq "$@"
 }
 
 inspec() {
     del_stopped inspec
     # shellcheck disable=SC2086
-    docker run \
-        $DOCKER_FLAGS \
+    docker run "$DOCKER_TTY_OPTION" \
+        -i \
         --net=host \
         -v /etc/localtime:/etc/localtime:ro \
         -v "$(pwd)":/share \
         -v "${HOME}"/.ssh:/root/.ssh:ro \
         --name inspec \
+        --rm \
         chef/inspec "$@"
 }
 
 liquibase() {
     del_stopped liquibase
     # shellcheck disable=SC2086
-    docker run \
-        $DOCKER_FLAGS \
+    docker run "$DOCKER_TTY_OPTION" \
+        -i \
         -v /etc/localtime:/etc/localtime:ro \
         --name liquibase \
+        --rm \
         ferrarimarco/liquibase "$@"
 }
 
 maven() {
     del_stopped maven
     # shellcheck disable=SC2086
-    docker run \
-        $DOCKER_FLAGS \
+    docker run "$DOCKER_TTY_OPTION" \
+        -i \
         -v /etc/localtime:/etc/localtime:ro \
         -v "${HOME}/.m2:/var/maven/.m2" \
         --name changelog-generator \
+        --rm \
         -u "$(id -u)":"$(id -g)" \
         -e MAVEN_CONFIG=/var/maven/.m2 \
         maven \
@@ -123,17 +129,17 @@ maven() {
 super_linter() {
     del_stopped super-linter
     # shellcheck disable=SC2086
-    docker run \
-        $DOCKER_FLAGS \
+    docker run "$DOCKER_TTY_OPTION" \
+        -i \
         --name super-linter \
+        --rm \
         -v "$(pwd)":/workspace \
         -w="/workspace" \
-        -e ACTIONS_RUNNER_DEBUG=true \
         -e DEFAULT_WORKSPACE=/workspace \
         -e DISABLE_ERRORS=false \
         -e LINTER_RULES_PATH=/workspace \
         -e MULTI_STATUS=false \
         -e RUN_LOCAL=true \
         -e VALIDATE_ALL_CODEBASE=true \
-        github/super-linter:V3.8.0
+        github/super-linter:V3.8.0 "$@"
 }
