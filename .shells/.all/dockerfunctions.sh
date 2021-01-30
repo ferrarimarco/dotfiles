216cbda7f26cfd29412c519b8cabad8100f90f9c
Marco Ferrari <ferrari.marco@gmail.com>
2021-01-30 11:55:59 +0100

    Update super-linter to v3.14.5

--- .shells/.all/dockerfunctions.sh
+++ .shells/.all/dockerfunctions.sh
@@ -138,9 +138,10 @@ super_linter() {
     -w="/workspace" \
     -e DEFAULT_WORKSPACE=/workspace \
     -e DISABLE_ERRORS=false \
+    -e ERROR_ON_MISSING_EXEC_BIT=true \
     -e LINTER_RULES_PATH=. \
     -e MULTI_STATUS=false \
     -e RUN_LOCAL=true \
     -e VALIDATE_ALL_CODEBASE=true \
-    ghcr.io/github/super-linter:v3.13.5 "$@"
+    ghcr.io/github/super-linter "$@"
 }
