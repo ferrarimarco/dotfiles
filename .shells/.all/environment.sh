415de3fa525f52f0675f6d545edaf08b247be9d1
Marco Ferrari <ferrari.marco@gmail.com>
2021-09-17 07:25:46 +0000

    Ensure that MANPATH variable is set

--- .shells/.all/environment.sh
+++ .shells/.all/environment.sh
@@ -79,7 +79,7 @@ if is_macos; then
     export PATH="${HOMEBREW_PATH}"/bin:"${PATH}"
     export PATH="${HOMEBREW_PATH}"/sbin:"${PATH}"
     export LD_LIBRARY_PATH="${HOMEBREW_PATH}"/lib:"${LD_LIBRARY_PATH-}"
-    export MANPATH="${HOMEBREW_PATH}"/share/man:"${MANPATH}"
+    export MANPATH="${HOMEBREW_PATH}"/share/man:"${MANPATH-}"
     export HOMEBREW_NO_ANALYTICS=1
 
     ZSH_SITE_FUNCTIONS_PATH="${HOMEBREW_PATH}"/share/zsh/site-functions
